60dcce395e5a8d24222a37a1da947b2265dcd42f
Nkhensani Mathebula <st10471843@vcconecct.edu.za>
2025-07-31 11:53:49 +0200

    first commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Good day! My Name is Nkhensani Mathebula and I develop apps.</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFCAD4',
     alignItems: 'center',
     justifyContent: 'center',
   },
